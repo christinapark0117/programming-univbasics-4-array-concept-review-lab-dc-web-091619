63ed9e307603de50420ba5b6a88a531f4c1ff00d
Christina Park <christinapark0117@gmail.com>
2019-09-13 20:00:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  current_index = 0
+  length = array.length
+    
+  while current_index < length do
+    if array[current_index] == value_to_find
+      return current_index
+    end 
+    current_index += 1
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
 end
