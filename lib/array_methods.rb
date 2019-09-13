1cabbb6f6136a80346067743c7cb7aee7ccf926c
Christina Park <christinapark0117@gmail.com>
2019-09-13 22:07:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,5 +24,14 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
+  current_index = 0
+  min_value = array[0]
   
+  while current_index < array.length do
+    if array[current_index] < min_value
+      min_value = array[current_index]
+    end
+    current_index += 1
+  end
+  return min_value
 end
